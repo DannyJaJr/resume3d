c5b3e862b0ed408fdfbeb94c48f8a0d3f56d81cf
Daniel Lafontant <ldanieljajr@gmail.com>
2021-10-13 20:20:20 -0400

    canvas created

--- main.js
+++ main.js
@@ -6,16 +6,35 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGL1Renderer({
+const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 })
 
 //adjeuct the camera
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.positionsetZ(30);
+camera.position.setZ(30);
 
 // render == Draw
 renderer.render(scene, camera);
+
+
+// now add object
+const geometry  = new THREE.TorusBufferGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });
+// now combie the geomtry with the material
+const torus = new THREE.Mesh( geometry, material);
+
+scene.add(torus)
+
+// function to renderer.render(scene, camera);
+function animate(){
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+
+}
+animate()
+
+
 
 
